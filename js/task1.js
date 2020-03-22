bd1755468c3c42a5ec801f59260f4d2ac8cf358e
Sota Sakamoto <sukimasuicchi@icloud.com>
2020-03-23 03:34:58 +0900

    first commit

---
+++ js/task1.js
@@ -0,0 +1,25 @@
+(function() {
+  'use strict';
+  const menu = document.getElementById('menu');
+  //ハンバーガーメニュー
+  menu.addEventListener('click', function() {
+    if (document.body.className != 'menu-open') {
+      document.body.className = 'menu-open';
+    } else {
+      document.body.className = '';
+    }
+  });
+  // setInterval(slide, 5000);
+  // const fadeIn = document.getElementsByClassName('fade-in');
+  // const windowHeight = window.parent.screen.height;
+  // //スクロールをトリガーにしたイベント
+  // window.addEventListener('scroll', function() {
+  //   //高さを取得して表示
+  //   for (let i = 0; i < fadeIn.length; i++) {
+  //     let fadeInY = fadeIn[i].getBoundingClientRect().top;
+  //     if (fadeInY < windowHeight - 200) {
+  //       fadeIn[i].classList.add('scroll-in');
+  //     }
+  //   }
+  // }, false);
+})();
